54f83d38efd779641399db6fac9350469c9c0f26
HordurThor <hordur20@ru.is>
2022-09-14 11:39:12 +0000

    fix models

--- src/app/STthLane/STthLane/settings.py
+++ src/app/STthLane/STthLane/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employees.apps.Lane17Config',
+    'employees.apps.EmployeesConfig',
+    'bookings.apps.BookingsConfig'
 ]
 
 MIDDLEWARE = [
--- src/app/STthLane/bookings/models.py
+++ src/app/STthLane/bookings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class lanes(models.Model):
+    lane_nr = models.IntegerField(primary_key=True)
+
+
+class bookings(models.Model):
+    lane_nr = models.ForeignKey(lanes, on_delete=models.CASCADE)
+    booker_phone = models.CharField(max_length=13, default="No phone")
+    start_time = models.DateTimeField(null=True)
+    end_date = models.DateTimeField(null=True)
+    payed = models.BooleanField(default=False)
