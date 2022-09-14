772f1bd72c23f7b1d749608dc1a8906967441bd4
HordurThor <hordur20@ru.is>
2022-09-14 11:25:03 +0000

    Revert "fixed empty module name og merge-aði lanes og booking í eitt app"
    This reverts commit 8c55cc9fff1caf7acf15a0e4f99e7268a1d2aff9.

--- src/app/STthLane/lanes/apps.py
+++ src/app/STthLane/lanes/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Lane17Config(AppConfig):
+class LanesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'employees'
+    name = 'lanes'
