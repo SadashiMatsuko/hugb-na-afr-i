aa6be208e67f82a786104b045265454a30a3f41f
brotherjoi <brotherjoi@gmail.com>
2022-09-17 09:21:28 +0000

    added tostring method and skeleton for view

--- src/app/STthLane/news/models.py
+++ src/app/STthLane/news/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from datetime import datetime
 
+
 # Create your models here.
 class articles(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
     date_posted = models.DateTimeField(default=datetime.now)
-    # author foreign key to employee on_delete = models.CASCADE
+    # author foreign key to employee/user on_delete = models.CASCADE
+    def __str__(self):
+        return self.title
--- src/app/STthLane/news/views.py
+++ src/app/STthLane/news/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from news.models import articles
 
 # Create your views here.
+def create_news(request):
+    if request.method == 'POST':
+        pass
+    else:
+        pass
